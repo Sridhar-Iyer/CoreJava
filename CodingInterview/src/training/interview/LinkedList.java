279be65ccf07ab8946a260498e1310c544c490d0
Sridhar <sreedharriyer@gmail.com>
2017-03-22 20:51:08 -0700

    Coding interview: LinkedList

--- CodingInterview/src/training/interview/LinkedList.java
+++ CodingInterview/src/training/interview/LinkedList.java
@@ -3,37 +3,77 @@ package training.interview;
 public class LinkedList {
 
 	public static void main(String[] args) {
-		
+
+	}
 }
 
-private class Node {
-	Object data;
-	Node next;
+class List {
 	
-	public Node(Object dataValue) {
-		next = null;
-		data = dataValue;		
+	private static int counter;
+	private Node head;
+	
+	public List() {
+		
 	}
 	
-	public Node(Object dataValue, Node nextValue) {
-		next = nextValue;
-		data = dataValue;
+	public void add(Object data) {
+		if (head == null) {
+			head = new Node(data);		
+		}
+		
+		Node temp = new Node(data);
+		Node current = head;
+		
+		if (current != null) {
+			while(current.getNext() != null) {
+				current = current.getNext();
+			}
+			current.setNext(temp);
+		}
+		
+		incrementCounter();
 	}
 	
-	public Object getValue() {
-		return data;
+	private static int getCounter() {
+		return counter;
 	}
 	
-	public void setData(Object dataValue) {
-		data = dataValue;
+	private static void incrementCounter() {
+		counter++;
 	}
 	
-	public Node getNext() {
-		return next;
+	private void decrementCounter() {
+		counter--;
 	}
 	
-	public void setNext(Node nextValue) {
-		next = nextValue;
+	private class Node {
+		Object data;
+		Node next;
+
+		public Node(Object dataValue) {
+			next = null;
+			data = dataValue;
+		}
+
+		public Node(Object dataValue, Node nextValue) {
+			next = nextValue;
+			data = dataValue;
+		}
+
+		public Object getValue() {
+			return data;
+		}
+
+		public void setData(Object dataValue) {
+			data = dataValue;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node nextValue) {
+			next = nextValue;
+		}
 	}
-}
 }
