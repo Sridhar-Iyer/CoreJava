60cfca074962d64b4d47bcbb7c2bef0d7e4cbd23
Sridhar <sreedharriyer@gmail.com>
2017-04-15 20:53:45 -0700

    Rally Health - Added problem

--- InterviewProblems/src/practice/interview/rallyhealth/QueueWithoutArrayOrList.java
+++ InterviewProblems/src/practice/interview/rallyhealth/QueueWithoutArrayOrList.java
@@ -1,5 +1,30 @@
 package practice.interview.rallyhealth;
 
-public class QueueWithoutArrayOrList {
+/**
+ * Assume in interview land that all lists are
+ * broken. Array ArrayList, LinkedList, SkipList
+ * Stacks, Queues, Dequeues etc. The Queuue 
+ * needs to meet the following interface.
+ * 
+ */
+
+interface Queue<T> {
+	public void enqueue(T t);
+	public T dequeue();
+}
+
+public class QueueWithoutArrayOrList<T> implements Queue<T> {
+
+	@Override
+	public void enqueue(T t) {
+		
+		
+	}
+
+	@Override
+	public T dequeue() {
+		
+		return null;
+	}
 
 }
