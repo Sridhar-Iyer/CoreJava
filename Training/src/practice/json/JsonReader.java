1cfd960791567aea2e03c2f33f8a2d751d40c868
Sridhar <sreedharriyer@gmail.com>
2018-12-03 17:02:15 +0530

    JSON: json reader

---
+++ Training/src/practice/json/JsonReader.java
@@ -0,0 +1,18 @@
+package practice.json;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import sample.model.Address;
+import sample.model.Employee;
+
+public class JSONReader {
+
+    
+}
+
